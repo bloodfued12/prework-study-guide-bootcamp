26c79b2a9c6e87f9e25cd9b3139990e4db9bdd12
bloodfued12 <benjamin.smith.ep@gmail.com>
2023-09-22 14:01:08 +0100

    accidental mix up

--- prework-study-guide/prework-study-guide/assets/script.js
+++ prework-study-guide/prework-study-guide/assets/script.js
@@ -1,6 +1,6 @@
 var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
 for (var x = 0; x < topics.length; x++) {
-    console.log(topics[x]);
+    console.log(topics[x]); 
 }
 /*
 if (topic === 'HTML') {
@@ -15,3 +15,4 @@ if (topic === 'HTML') {
  console.log('Please try again!');
 }
 */
+var 
